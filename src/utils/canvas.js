e48c716497800b50838b8997f52d6e3c7a5983ac
Usman Siddiqui <techbytehelper@googlemail.com>
2018-05-22 10:42:13 +0100

    Draw virtual vehicles as lines

--- src/utils/canvas.js
+++ src/utils/canvas.js
@@ -23,25 +23,36 @@ class Canvas {
 	}
 	
 	renderClock(simulationTime) {
+		this.ctx.fillStyle = 'rgba(0, 0, 0, 0.9)';
 		this.ctx.font = '10px serif';
 		this.ctx.textBaseline = 'top';
 		this.ctx.fillText(simulationTime.toFixed(2), 0, 0);
 	}
 	
 	renderVehicle(vehicle) {
-		if (vehicle.isSpawned === true && vehicle.isVirtual === false) {
+		if (vehicle.isSpawned === true) {
 			const position = vehicle.position;
-			const velocity = vehicle.velocity;
 			const x = (position / MAX_ROAD_LENGTH) * this.cc.width;
-			const y = this.cc.height / 2;
-			const radius = Math.max(Math.sqrt(velocity), 2); /* Draws faster cars as being larger for visual effect */
-			const startAngle = 0;
-			const endAngle = 2 * Math.PI;
-		
-			this.ctx.fillStyle = 'rgba(0, 0, 0, 0.9)';
-			this.ctx.beginPath();
-			this.ctx.arc(x, y, radius, startAngle, endAngle);
-			this.ctx.fill();
+			
+			if (vehicle.isVirtual === true) {
+				this.ctx.strokeStyle = 'rgba(229, 57, 53, 1.0)';
+				this.ctx.beginPath();
+				this.ctx.moveTo(x, 10);
+				this.ctx.lineTo(x, 25);
+				this.ctx.stroke();
+			} else {
+				const velocity = vehicle.velocity;
+				const y = this.cc.height / 2;
+				const radius = Math.max(Math.sqrt(velocity), 2); /* Draws faster cars as being larger for visual effect */
+				const startAngle = 0;
+				const endAngle = 2 * Math.PI;
+				
+				this.ctx.strokeStyle = 'rgba(0, 0, 0, 0.9)';
+				this.ctx.fillStyle = 'rgba(0, 0, 0, 0.9)';
+				this.ctx.beginPath();
+				this.ctx.arc(x, y, radius, startAngle, endAngle);
+				this.ctx.fill();
+			}
 		}
 	}
 }
